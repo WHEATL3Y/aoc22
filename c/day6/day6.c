d1da3ef7aa8f3567d914d91acd0da614602c238b
Jacob Christensen <jchristensen888@gmail.com>
2023-08-11 10:29:34 -0600

    AOC - C - DAY6

--- c/day6/day6.c
+++ c/day6/day6.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 int checkDistinct(unsigned, int);
+int getMarker(int *, int, int);
 
 int checkDistinct(unsigned value, int n) {
 
@@ -27,20 +28,50 @@ int checkDistinct(unsigned value, int n) {
 
 }
 
+int getMarker(int *buffer, int mSize, int bSize) {
+
+    int low = 0;
+    int high = mSize - 1;
+    int i = 0;
+
+    unsigned value = 0;
+    unsigned mask = 1;
+
+    while (high <= bSize) {
+
+        for (i = low; i <= high; i++) {
+            mask <<= buffer[i]; 
+            value |= mask;
+            mask = 1;
+        }
+     
+        low++;
+        high++;
+
+        if (checkDistinct(value, mSize)) {
+            return high;
+        } 
+        else {
+            value = 0;
+        }
+
+    }
+
+    return 0;
+
+}
+
 int main(void) {
 
     int answer1;
     int answer2;
 
-    FILE *f = fopen("sample.txt", "r");
+    FILE *f = fopen("input.txt", "r");
     int *string;
     int size;
     int c;
 
-    int low = 0;
-    int high = 3;
-    unsigned mask = 0;
-    unsigned value = 0;
+    int i = 0;
   
     fseek(f, 0, SEEK_END);
     size = ftell(f);
@@ -49,10 +80,17 @@ int main(void) {
     string = malloc(size * sizeof(int));
 
     while ((c = getc(f)) != EOF) {
-        *string++ = c - 'a';
+        string[i++] = c - 'a';
     }
 
-    printf("TEST size: %d\n", size);
-    printf("TEST checkDistinct: %d\n", checkDistinct(778, 4));
+    answer1 = getMarker(string, 4, size);
+    answer2 = getMarker(string, 14, size);
+
+    /* printf("TEST size: %d\n", size); */
+    /* printf("TEST checkDistinct: %d\n", checkDistinct(778, 4)); */
+
+    printf("%d\n%d\n", answer1, answer2);
+
     return EXIT_SUCCESS;
+
 }
